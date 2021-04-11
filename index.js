9f4c69d0e0dce4ea94f9481c4c6e42187ff4ec2c
Caitlin Raby <caitlinraby@gmail.com>
2021-04-11 08:41:03 -0500

    commit tree f1ecb28e9b2a52439b41ac11409a3c2dd27ab990

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function(num1=200, num2=10) {
+    return num1 / num2;
+}
+
+const square = (param1) => param1 * param1;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
